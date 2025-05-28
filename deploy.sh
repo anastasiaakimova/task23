89ff6a25bd61561d40e6cf77e0481ceebaf3c752
Anastasia <anastasia.akimova.v@gmail.com>
2025-05-28 18:02:27 +0300

    Create deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+docker stop task23-container || true
+
+docker rm task23-container || true
+
+docker pull anastasiaakimova/task23:1.0
+
+docker run -d \
+  --name task23-container \
+  -p 80:3000 \
+  anastasiaakimova/task23:1.0
+
+docker ps | grep task23-container
